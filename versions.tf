3c6b47af94b6f1f1e9f5b1efee33e86ddefe05cd
sbglfj <jpx2b1@gmail.com>
2025-06-30 11:44:17 -0400

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,10 @@
+// Update versions as appropriate for time of reading
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    grafana = {
+      source  = "grafana/grafana"
+      version = "1.34.0"
+    }
+  }
+}
