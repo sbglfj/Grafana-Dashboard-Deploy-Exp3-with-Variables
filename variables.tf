743e394146ec263c308546fa21bc0d6776968a4a
sbglfj <jpx2b1@gmail.com>
2025-06-30 11:50:16 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "grafana_dashboard_folder_name" {
+  description = "Folder name created on grafana instance"
+  type        = string
+  default     = "test-dashboard-folder-via-var"
+}
+
+variable "dashboard_file_path" {
+  description = "Grafana dashboard file  local path"
+  type        = string
+  default     = "dashboards/"
+}
+variable "grafana_endpoint" {
+  type        = string
+  description = "Define Endpoint of Grafana"
+  default     = "https://sbglfj.grafana.net"
+}
+
+
+variable "grafana_service_account_api_key" {
+  type        = string
+  description = "Define API key to conect Grafana instance"
+  default     = "XXXXXXXXXXXXXXXX"
+}
